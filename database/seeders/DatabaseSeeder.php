64c10ad9cc3e46ba13fb1005ae4653ccafa80518
digital opium <test@ismail.com>
2023-06-16 17:59:14 +0100

    start...

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Serveur;
+use App\Models\Test;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
